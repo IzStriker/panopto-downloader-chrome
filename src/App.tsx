534eb1a35bbb5ef5abe3b8a9fe43181f32608eac
IzStriker <zjac104@live.rhul.ac.uk>
2022-10-02 22:31:13 +0100

    chore: purge

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
-import React from 'react';
-import './App.css';
-import { Alert, Grid } from '@mui/material';
-import TitleComponent from './components/TitleComponent/TitleComponent';
-import MainSelectorComponent from './components/MainSelectorComponent/MainSelectorComponent';
-import FooterComponent from './components/FooterComponent/FooterComponent';
-import HorizontalRule from './components/HorizontalRule/HorizontalRule';
-import { initialiseDownloadManagerHandler } from './DownloadManagerHandler';
-import DownloadManagerPage from './components/DownloadManagerPage/DownloadManagerPage';
+import React from "react";
+import "./App.css";
+import { Alert, Grid } from "@mui/material";
+import TitleComponent from "./components/TitleComponent/TitleComponent";
+import MainSelectorComponent from "./components/MainSelectorComponent/MainSelectorComponent";
+import FooterComponent from "./components/FooterComponent/FooterComponent";
+import HorizontalRule from "./components/HorizontalRule/HorizontalRule";
 
 function App() {
-    try {
-        initialiseDownloadManagerHandler();
-    }
-    catch (error) {
-        console.error(error);
-    }
-
-    return (
-        <Grid id='main' container>
-            <TitleComponent />
-            <Alert severity="warning" style={{ textAlign: "left" }}>This version of Panopto Downloader is currently in development. Some features may not work as expected.</Alert>
-            <MainSelectorComponent />
-            <HorizontalRule />
-            <FooterComponent />
-            <DownloadManagerPage />
-        </Grid>
-    );
+  return (
+    <Grid id="main" container>
+      <TitleComponent />
+      <Alert severity="warning" style={{ textAlign: "left" }}>
+        This version of Panopto Downloader is currently in development. Some features may not work
+        as expected.
+      </Alert>
+      <MainSelectorComponent />
+      <HorizontalRule />
+      <FooterComponent />
+    </Grid>
+  );
 }
 
 export default App;
--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BackgroundPage from './BackgroundPage';
-import { createTheme , responsiveFontSizes, ThemeProvider } from '@mui/material';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createTheme, responsiveFontSizes, ThemeProvider } from "@mui/material";
 
-const theme = responsiveFontSizes(createTheme({
+const theme = responsiveFontSizes(
+  createTheme({
     palette: {
-        primary: {
-            main: '#2e7d32'
-        },
-        secondary: {
-            main: '#ffc107'
-        }
+      primary: {
+        main: "#2e7d32",
+      },
+      secondary: {
+        main: "#ffc107",
+      },
     },
     typography: {
-        fontSize: 12,
-        body1: {
-            lineHeight: "normal",
-            letterSpacing: 0
-        },
-        body2: {
-            fontSize: 14,
-            fontWeight: 500,
-            lineHeight: "normal",
-            letterSpacing: 0
-        },
-        button: {
-            letterSpacing: '0.04em',
-            fontSize: 14,
-            fontWeight: 500
-        }
-    }
-}), {});
+      fontSize: 12,
+      body1: {
+        lineHeight: "normal",
+        letterSpacing: 0,
+      },
+      body2: {
+        fontSize: 14,
+        fontWeight: 500,
+        lineHeight: "normal",
+        letterSpacing: 0,
+      },
+      button: {
+        letterSpacing: "0.04em",
+        fontSize: 14,
+        fontWeight: 500,
+      },
+    },
+  }),
+  {}
+);
 
 ReactDOM.render(
-    <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <BackgroundPage />
-        </ThemeProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <ThemeProvider theme={theme}></ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
